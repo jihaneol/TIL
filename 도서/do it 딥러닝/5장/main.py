5f14d860375a5133b858bb08710800409f8c332b
jihaneol <6531z@naver.com>
2025-06-03 21:03:04 +0900

    딥러닝 5장 데이터 전처리

---
+++ 도서/do it 딥러닝/5장/main.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from SingleLayer import Single
+import matplotlib.pyplot as plt
+import numpy as np
+
+cancer = load_breast_cancer()
+x = cancer.data
+y = cancer.target
+
+x_train_all, x_test, y_train_all, y_test = train_test_split(x,y,stratify=y, test_size=0.2, random_state=42)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train_all, y_train_all, stratify=y_train_all, test_size=0.2, random_state=42)
+
+print(cancer.feature_names[[2,3]])
+plt.boxplot(x_train[:, 2:4])
+plt.xlabel('feature')
+plt.ylabel('value')
+plt.show()
+
+print(x_train.shape[1])
+layer = Single()
+layer.fit(x_train, y_train)
+layer.score(x_val, y_val)
